f7d1a75c2c923a40ab5b4d6be9cae1f60709267f
freddy913 <161123433+freddy913@users.noreply.github.com>
2026-02-02 22:31:20 +0100

    Add functions for multiplication and factorial

---
+++ target.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int multiply_numbers(int a, int b) {
+    return a * b;
+}
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return multiply_numbers(n, factorial(n-1));
+}
+
+
+
+int main() {
+   
+    int fact = factorial(5);
+    int prod = multiply_numbers(5, 10);
+
+    printf("factorial: %d, product: %d\n",fact, prod);
+    
+    return 0;
+}
